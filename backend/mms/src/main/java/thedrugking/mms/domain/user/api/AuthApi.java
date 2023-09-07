5896f7325ebf3cd60ae420cdf18a73cb60622bcf
barami62 <wodbs7758@naver.com>
2023-09-07 13:22:47 +0900

    feat : 회원가입 엔드 포인트 추가

---
+++ backend/mms/src/main/java/thedrugking/mms/domain/user/api/AuthApi.java
@@ -0,0 +1,29 @@
+package thedrugking.mms.domain.user.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import thedrugking.mms.domain.user.application.UserAuthService;
+import thedrugking.mms.domain.user.dto.SignUpRequestDto;
+import thedrugking.mms.global.common.response.SuccessResponseDto;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthApi {
+
+	private final UserAuthService userAuthService;
+
+	@PostMapping("/api/signup")
+	public ResponseEntity<SuccessResponseDto> signup(@Valid SignUpRequestDto request) {
+
+		log.info("req = {}", request.getName());
+
+		SuccessResponseDto success = userAuthService.signup(request);
+		return ResponseEntity.ok(success);
+	}
+}
